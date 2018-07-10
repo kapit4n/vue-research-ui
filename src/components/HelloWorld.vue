c03d7cc6b23d547e0ac1774db5ba859e839c1ce2
Luis Arce <luis.arce22@gmail.com>
2018-07-10 11:43:43 -0400

    Init project with vue js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,10 +30,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'HelloWorld'
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Research from './components/Research.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: HelloWorld },
+  { path: '/research', component: Research }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 }).$mount('#app')
